a5e87648dc6a3d90d833996f6be9313572c6e574
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2021-10-10 15:42:41 +0530

    changed home file

--- src/components/top-fold/index.js
+++ src/components/top-fold/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, {  useState } from "react";
+import { Link} from "react-router-dom";
 import "./top-fold.css";
 import { useDispatch } from "react-redux";
 import { searchExpense } from "../../redux/actions/expenses";
